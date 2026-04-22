fa2d0f70c7c75f0b700f11958a3fa785422e27bc
vitinhog08 <pereira.vitor03@escola.pr.gov.br>
2026-04-22 16:26:15 -0300

    23

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const botoes=document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 
-for (let i=0; i <= botoes.length;i++) {
-    botoes[i].onclick=function(){
+for (let i = 0; i <= botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for (let j=0; i <= botoes.length;j++) {
-    botoes[i].classList.remove=("ativo");{
-
-        botoes[i].classList.add("ativo");
+        for (let j = 0; i <= botoes.length; j++) {
+            botoes[i].classList.remove = ("ativo"); {
+            }
+            botoes[i].classList.add("ativo");
         }
-         console.log(botoes[i]);
-    } 
-}
+    }
